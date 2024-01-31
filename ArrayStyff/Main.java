49a47076324e6f3fd2996fa0aa573bba0bfc7793
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-01-31 21:15:29 +0200

    Update Main.java

--- ArrayStyff/Main.java
+++ ArrayStyff/Main.java
@@ -32,7 +32,7 @@ public class Main {
                             System.out.println("What tasks do you want to add?");
                             String addTask = "";
                             addTask = scanner.nextLine();
-                            backCode.addCapsuleStuff(addTask);
+                            backCode.setCapsuleStuff(addTask);
                         }
                         
                         // print the array
@@ -41,20 +41,22 @@ public class Main {
 
                     case 2: // remove task
                         // remove from the array, let the user choose what items to remove
-                        System.out.println("How many tasks do you want to remove?");
+                        System.out.println("How many tasks do you want to remove? Note that you can only remove 1 item at a time!");
                         int remove = scanner.nextInt();
                         scanner.nextLine();
 
                         if (remove > backCode.getCapsuleStuff().size()) {
                             System.out.println("You can't remove more than you have!");
                             System.exit(0);
-                        } else if (remove > 0) {
+                        } else if ((remove > 0 && remove <=1) && (backCode.getCapsuleStuff().size() > 0)){
                             System.out.println("Task to remove: ");
                             int index = scanner.nextInt();
                             scanner.nextLine();
                             for (int i = 0; i < remove; i++) {
                                 backCode.removeCapsuleStuff(index);
                             }
+                        } else if( remove > 1 ){
+                            System.out.println("You can't remove more than 1 item at a time");
                         } else {
                             System.out.println("You can't remove 0 items!");
                         }
@@ -64,28 +66,12 @@ public class Main {
                         break;
                     
                     case 3: //edit task
-                        // edit the array, let the user choose what items to edit
-                        System.out.println("How many tasks do you want to edit?");
-                        int edit = scanner.nextInt();
+                        System.out.println("What item do you want to edit?");
+                        int index = scanner.nextInt();
                         scanner.nextLine();
-                        
-                        if (edit > backCode.getCapsuleStuff().size()) {
-                            System.out.println("You can't edit more than you have!");
-                            System.exit(0);
-                        } else if (edit > 0) {
-                            System.out.println("What item do you want to edit?");
-                            int index = scanner.nextInt();
-                            scanner.nextLine();
-                            for (int i = 0; i < edit; i++) {
-                                System.out.println("Task : ");
-                                String editItem = scanner.nextLine();
-                                backCode.editCapsuleStuff(index, editItem);
-                            }
-                        } else {
-                            System.out.println("You can't edit 0 items!");
-                            
-                        }
-
+                        System.out.println("Enter the new task: ");
+                        String edit = scanner.nextLine();
+                        backCode.editCapsuleStuff(index, edit);
                         // print the array
                         System.out.println(backCode.getCapsuleStuff());
                         if (backCode.getCapsuleStuff().size() == 0) {
